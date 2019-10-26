afa2d06a9ebc80e50dda7ebce7d20301f7bd218a
Yining Wang <yining.wang@utexas.edu>
2019-10-26 02:19:36 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,11 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp nds
 end
 
 def print_first_directors_movie_titles
+  for item in directors_database[0][:movies] do
+    puts "#{item[:title]}\n"
+  end
 end
